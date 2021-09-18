5bdd030d3d8633e888f42df4fdb199024dbd11b0
DDLD93 <umar.jere@email.com>
2021-09-18 02:20:41 +0100

    fixed news audio link

--- fetchers/newsAudio.js
+++ fetchers/newsAudio.js
@@ -8,14 +8,14 @@ function fetchVOA(link) {
   .on('error', function(err) {
     console.log(err)
   })
-  .pipe(fs.createWriteStream(`./news/VOA_Hausa.mp3`));
+  .pipe(fs.createWriteStream(`./news/voa.mp3`));
 }
 
 function newsAudio() {
   // new Date object
 let date = new Date();
 let year = date.getFullYear()
-let day = date.getDate()
+let day =  date.getDate()
 let month = date.getMonth()
 let hour = date.getHours()
 if(day <= 9) {
@@ -29,16 +29,18 @@ if(month <= 9) {
 }
 
 var linkHatsi = `https://av.voanews.com/clips/VHA/${year}/${month}/${day}/${year}${month}${day}-203000-VHA066-program_48k.mp3`
-var linkSafe = `https://av.voanews.com/clips/VHA/${year}/${month}/${day}/${year}${month}${day}-05000-VHA066-program_48k.mp3`
- 
+var linkSafe = `https://av.voanews.com/clips/VHA/${year}/${month}/${day}/${year}${month}${day}-050000-VHA066-program_48k.mp3`
+ console.log(linkHatsi)
+ console.log(linkSafe)
 if (hour > 6 && hour < 21) {
   fetchVOA(linkSafe)
 }else{
   fetchVOA(linkHatsi)
 }
 }
+//newsAudio()
 setInterval(() => {
   newsAudio()
-}, 1000 * 60 * 60);
+}, 1000 * 60 * 360);
 
 module.exports = {newsAudio}
